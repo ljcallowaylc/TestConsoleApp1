ccd741910e491000ca7d7d43162d9557b17ae10e
ljcal <ljcal@DESKTOP-BGQG14U>
2025-12-02 18:05:32 -0600

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int> {1, 5, 7, 2, 1, 10};
+
+            var even = numbers.Where(x => x % 2 == 0);
+
+            foreach (var e in even)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
